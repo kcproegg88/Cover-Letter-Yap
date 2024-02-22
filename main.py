e94ecd848df5b0501e8fac69431edc6549bc19ab
krishna <krischauhan01@gmail.com>
2024-02-21 21:07:18 -0500

    completed working version

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import os
-import functions
-import random
+import txtfile
 
-main_folder = "/user_folder"  # can edit the valid
+main_folder = "user_name"  # can edit the foldername
 num_paragraphs = 1
-identifier = "["
+flag = "["
+keywords = {"name]": "your name", "company]": "github", "position]": "Software Engineer"}
 
 def main():
-    num_paragraphs = 1
-    sections = ["/introduction"]
-    #[sections.append("/paragraph_"+str(i)) for i in range(num_paragraphs)]
-    #sections.append("/conclusion")
-    for folders in sections:
-        options = os.listdir(main_folder+folders)
-        i = random.randint(1,len(options))
-        with open(main_folder+folders+"/"+str(i)+".txt") as file:
-            print(file)
+    num_paragraphs = 2
+    sections = ["introduction"]
+    [sections.append("paragraph_"+str(i+1)) for i in range(num_paragraphs)]
+    sections.append("conclusion")
+
+    txtfile.combine(sections, main_folder, "output.txt", flag, keywords)
+
+    input()
+    txtfile.clear("output.txt")
 
 
 if __name__ == "__main__":
---
+++ txtfile.py
@@ -0,0 +1,44 @@
+import os
+import random
+
+
+def combine(sections, main_folder, output, flag, keywords):
+    for folder in sections:
+        folder_path = os.path.join(main_folder, folder)
+        options = os.listdir(folder_path)
+        i = random.randint(1, len(options))
+        file_path = os.path.join(folder_path, str(i)+".txt")
+        try:
+            append(output, read(file_path, flag, keywords))
+        except FileNotFoundError:
+            print(f"Error: '{file_path}' not found")
+
+
+def read(filename, flag, keywords):
+    with open(filename, 'r') as file:
+        file_contents = key_words(file.read(), flag, keywords)
+        print(file_contents, "\n")
+        return file_contents
+
+
+def key_words(content, flag, keywords):
+    edited_content = []
+    for word in content.split():
+        if word[0] == flag:
+            if word[1:] in list(keywords.keys()):
+                word = keywords[word[1:]]
+            elif word[1:-1] in list(keywords.keys()):  # this one is for potential punctuation
+                word = keywords[word[1:-1]] + word[-1]
+        edited_content.append(word)
+    return ' '.join(edited_content)
+
+
+def append(filename, new_contents):
+    with open(filename, 'a') as file:
+        file.write(new_contents)
+        file.write("\n")
+
+
+def clear(filename):
+    with open(filename, 'w'):
+        pass
